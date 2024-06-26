8d972f73574dc99b862b569ca201dc1e3348a1e2
Rafael Fabri Chimidt <rafaelfabrichimidt@pop-os.localdomain>
2024-06-25 21:01:07 -0300

    testando

---
+++ teste/app.py
@@ -0,0 +1,11 @@
+from restaurante_final import Restaurante
+
+def main():
+    restaurante_praca = Restaurante('praca', 'gourmet')
+    restaurante_praca = Restaurante('Mexican Food', 'Mexicana')
+    restaurante_praca = Restaurante('Japa', 'Japonesa')
+
+
+
+if __name__=='__main__':
+    main()
